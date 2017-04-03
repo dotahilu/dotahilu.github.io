85671d39f74aa4057a910156b437ec1926636800
Phạm Anh Quốc <phamanhquoc2014@gmail.com>
2017-04-03 10:47:28 +0700

    Update template.js

--- js/template.js
+++ js/template.js
@@ -16,7 +16,7 @@
 		//-----------------------------------------------
 		$(window).scroll(function() {
 			if (($(".header.fixed").length > 0)) { 
-				if(($(this).scrollTop() > 0) && ($(window).width() > 200)) {
+				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
 					$("body").addClass("fixed-header-on");
 				} else {
 					$("body").removeClass("fixed-header-on");
@@ -26,7 +26,7 @@
 
 		$(window).load(function() {
 			if (($(".header.fixed").length > 0)) { 
-				if(($(this).scrollTop() > 0) && ($(window).width() > 200)) {
+				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
 					$("body").addClass("fixed-header-on");
 				} else {
 					$("body").removeClass("fixed-header-on");
@@ -67,7 +67,7 @@
 			$("[data-animation-effect]").each(function() {
 				var $this = $(this),
 				animationEffect = $this.attr("data-animation-effect");
-				if(Modernizr.mq('only all and (min-width: 201px)') && Modernizr.csstransitions) {
+				if(Modernizr.mq('only all and (min-width: 768px)') && Modernizr.csstransitions) {
 					$this.appear(function() {
 						setTimeout(function() {
 							$this.addClass('animated object-visible ' + animationEffect);
