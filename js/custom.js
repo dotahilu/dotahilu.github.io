e5f6227be418ae66732c7767e6615a715ee7e487
MinhHieu1512157 <hunterhieu97@gmail.com>
2017-04-08 15:53:54 +0700

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,28 @@
  * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
  * File Description: Place here your custom scripts
  */
+
+(function($){
+	$(document).ready(function(){
+		var slide_number = 1;
+	    $(".slide-left").click(function(){
+	        //event.preventDefault();
+	        if ( slide_number == 2) {
+                $(".process-for-center").fadeOut();
+                $(".process-for-seeker").fadeIn();
+                slide_number = 1;
+                $(".num-slide").text("1/2");
+            }
+        });
+        $(".slide-right").click(function(){
+        	//event.preventDefault();
+            if ( slide_number == 1) {
+                $(".process-for-seeker").fadeOut();
+                $(".process-for-center").fadeIn();
+                slide_number = 2;
+                $(".num-slide").text("2/2");
+            }
+        });
+		
+	}); // End document ready
+})(this.jQuery);
